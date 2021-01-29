b24271eafb7961a752db3058d08f2f0e420367fb
Kailun chen <kchen@parasoft.com>
2021-01-28 18:46:33 -0800

    mock ctp in tests

--- __tests__/main.test.ts
+++ __tests__/main.test.ts
@@ -1,18 +1,75 @@
 import * as process from 'process'
 import * as cp from 'child_process'
 import * as path from 'path'
+import * as core from '@actions/core';
+import * as main from '../src/main';
+import * as service from '../src/service';
+import q from 'q';
+import http from 'http';
 
-// shows how the runner will run a javascript action with env / stdout protocol
-test('test runs', () => {
-  process.env['INPUT_CTPURL'] = 'http://104.42.225.105/em'
-  process.env['INPUT_CTPUSERNAME'] = 'admin'
-  process.env['INPUT_CTPPASSWORD'] = 'admin'
-  process.env['INPUT_SYSTEM'] = 'ParaBank'
-  process.env['INPUT_ENVIRONMENT'] = 'TestDestroy'
-  const np = process.execPath
-  const ip = path.join(__dirname, '..', 'lib', 'main.js')
-  const options: cp.ExecFileSyncOptions = {
-	env: process.env
-  }
-  console.log(cp.execFileSync(np, [ip], options).toString())
-})
+describe('Test basic destroy environment action', () => {
+  const envId = 29;
+  const systemResponse_SUCCESS = {"systems": [{"id": 10, "name": "fake-system"}]};
+  const environmentResponse_SUCCESS = {"environments": [ {"id": envId, "name": "fake-environment", "systemId": 10 }]}
+  const deleteReponse_SUCCESS =  {"id": envId, "name": "fake-environment", "systemId": 10 }
+
+  test('test basic deploy environment', () => {
+    jest.spyOn(core, 'getInput').mockImplementation((val) => {
+      if (val === 'ctpUrl') {
+        return 'https://fake-ctp-endpoint:8080/em/'
+      } else if (val === 'ctpUsername' || val === 'ctpPassword') {
+        return 'secret';
+      } else if (val === 'systemName') {
+        return 'fake-system'
+      } else if (val === 'environmentName') {
+        return 'fake-environment'
+      }
+    });
+    jest.spyOn(service.WebService.prototype, 'findInEM').mockImplementation((path, property, name) => {
+        let def = q.defer();
+        let promise = new Promise((resolve, reject) => {
+            def.resolve = resolve;
+            def.reject = reject;
+        });
+        console.log('findInEM invoked');
+        let res = new http.IncomingMessage(null);
+        res.statusCode = 200;
+        if (path === '/api/v2/systems') {
+          def.resolve(systemResponse_SUCCESS);
+        }
+        else if (path === '/api/v2/environments') {
+          def.resolve(environmentResponse_SUCCESS);
+        }
+        return promise;
+    });
+    jest.spyOn(service.WebService.prototype, 'deleteFromEM').mockImplementation((path) => {
+        let def = q.defer();
+        let promise = new Promise((resolve, reject) => {
+            def.resolve = resolve;
+            def.reject = reject;
+        });
+        console.log('deleteFromEM invoked');
+        let res = new http.IncomingMessage(null);
+        res.statusCode = 200;
+        if (path === '/api/v2/environments/' + envId + '?recursive=true') {
+          def.resolve(deleteReponse_SUCCESS);
+        }
+        return promise;
+    });
+    main.run();
+  });
+  
+//    test('test runs', () => {
+//      process.env['INPUT_CTPURL'] = 'http://104.42.225.105/em'
+//      process.env['INPUT_CTPUSERNAME'] = 'admin'
+//      process.env['INPUT_CTPPASSWORD'] = 'admin'
+//      process.env['INPUT_SYSTEM'] = 'ParaBank'
+//      process.env['INPUT_ENVIRONMENT'] = 'TestDestroy'
+//      const np = process.execPath
+//      const ip = path.join(__dirname, '..', 'lib', 'main.js')
+//      const options: cp.ExecFileSyncOptions = {
+//      env: process.env
+//      }
+//      console.log(cp.execFileSync(np, [ip], options).toString())
+//    });
+});
---
+++ src/service.ts
@@ -0,0 +1,165 @@
+import * as core from '@actions/core'
+import http = require('http');
+import https = require('https');
+import q = require('q');
+import url = require('url');
+
+export interface Authorization {
+    username: string,
+    password: string
+}
+
+export class WebService {
+    private baseURL: url.Url;
+    protected protocol: typeof https | typeof http;
+    protected protocolLabel: string;
+    protected authorization: Authorization;
+
+    constructor(endpoint: string, context: string, authorization?: Authorization) {
+        this.baseURL = url.parse(endpoint);
+        if (this.baseURL.path === '/') {
+            this.baseURL.path += context;
+        } else if (this.baseURL.path === `/${context}/`) {
+            this.baseURL.path = `/${context}`;
+        }
+        this.authorization = authorization;
+        this.protocol = this.baseURL.protocol === 'https:' ? https : http;
+        this.protocolLabel = this.baseURL.protocol || 'http:';
+    }
+
+    getFromEM<T>(path: string) : Promise<T> {
+        let def = q.defer<T>();
+        let promise = new Promise<T>((resolve, reject) => {
+            def.resolve = resolve;
+            def.reject = reject;
+        });
+        let options = {
+            host: this.baseURL.hostname,
+            port: this.baseURL.port,
+            path: this.baseURL.path + path,
+            auth: undefined,
+            headers: {
+                'Accept': 'application/json'
+            }
+        }
+        if (this.protocolLabel === 'https:') {
+            options['rejectUnauthorized'] = false;
+            options['agent'] = false;
+        }
+        if (this.authorization) {
+            options.auth = this.authorization.username + ':' +  this.authorization.password;
+        }
+        console.log('GET ' + this.protocolLabel + '//' + options.host + ':' + options.port + options.path);
+        let responseString = "";
+        this.protocol.get(options, (res) => {
+            res.setEncoding('utf8');
+            res.on('data', (chunk) => {
+                responseString += chunk;
+            });
+            res.on('end', () => {
+                console.log('    response ' + res.statusCode + ':  ' + responseString);
+                let responseObject = JSON.parse(responseString);
+                def.resolve(responseObject);
+            });
+        }).on('error', (e) => {
+            def.reject(e);
+        });
+        return promise;
+      };
+      
+      findInEM<T>(path: string, property: string, name: string, otherProperty?:string, otherMatch?: number) : Promise<T>{
+        let def = q.defer<T>();
+        let promise = new Promise<T>((resolve, reject) => {
+            def.resolve = resolve;
+            def.reject = reject;
+        });
+        let options = {
+            host: this.baseURL.hostname,
+            port: this.baseURL.port,
+            path: this.baseURL.path + path,
+            auth: undefined,
+            headers: {
+                'Accept': 'application/json'
+            }
+        }
+        if (this.protocolLabel === 'https:') {
+            options['rejectUnauthorized'] = false;
+            options['agent'] = false;
+        }
+        if (this.authorization) {
+          options.auth = this.authorization.username + ':' +  this.authorization.password;
+        }
+        console.log('GET ' + this.protocolLabel + '//' + options.host + ':' + options.port + options.path);
+        let responseString = "";
+        this.protocol.get(options, (res) => {
+            res.setEncoding('utf8');
+            res.on('data', (chunk) => {
+                responseString += chunk;
+            });
+            res.on('end', () => {
+                console.log('    response ' + res.statusCode + ':  ' + responseString);
+                let responseObject = JSON.parse(responseString);
+                if (typeof responseObject[property] === 'undefined') {
+                    def.reject(property + ' does not exist in response object from ' + path);
+                    return;
+                }
+                for (let i = 0; i < responseObject[property].length; i++) {
+                    if (otherProperty && otherMatch) {
+                        if (responseObject[property][i][otherProperty] !== otherMatch) {
+                            continue;
+                        }
+                    }
+                    if (responseObject[property][i].name === name) {
+                        def.resolve(responseObject[property][i]);
+                        return;
+                    }
+                }
+                def.reject('Could not find name "' + name + '" in ' + property + ' from ' + path);
+                return;
+            });
+        }).on('error', (e) => {
+            def.reject(e);
+        });
+        return promise;
+    }
+      
+    deleteFromEM<T>(path: string) : Promise<T>{
+        let def = q.defer<T>();
+        let promise = new Promise<T>((resolve, reject) => {
+            def.resolve = resolve;
+            def.reject = reject;
+        });
+        let options = {
+            host: this.baseURL.hostname,
+            port: this.baseURL.port,
+            path: this.baseURL.path + path,
+            auth: undefined,
+            headers: {
+                'Accept': 'application/json'
+            }
+        }
+        if (this.protocolLabel === 'https:') {
+            options['rejectUnauthorized'] = false;
+            options['agent'] = false;
+        }
+        if (this.authorization) {
+          options.auth = this.authorization.username + ':' +  this.authorization.password;
+        }
+        console.log('DELETE ' + this.protocolLabel + '//' + options.host + ':' + options.port + options.path);
+        let responseString = "";
+        this.protocol.get(options, (res) => {
+            res.setEncoding('utf8');
+            res.on('data', (chunk) => {
+                responseString += chunk;
+            });
+            res.on('end', () => {
+                console.log('    response ' + res.statusCode + ':  ' + responseString);
+                let responseObject = JSON.parse(responseString);
+                def.resolve(responseObject);
+            });
+        }).on('error', (e) => {
+            def.reject(e);
+        });
+        return promise;
+    }
+} 
